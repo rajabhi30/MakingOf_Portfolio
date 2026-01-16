d93a344637c2e209244e311dd40b3d1c57b0e7ee
Abhishek kumar <rajabhishek8070@gmail.com>
2026-01-16 18:47:20 +0530

    Fix SkillContext JSX and asset imports

--- src/Context/SkillContext.jsx
+++ src/Context/SkillContext.jsx
@@ -1,12 +1,12 @@
 import { createContext } from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
+export const scontext = createContext();
+
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const scontext = createContext();
 
 const skills = [
   {
